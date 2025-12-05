1445825cf298eacf6762f83013a0d2758099f066
Arne Kiesewetter <banane9@live.de>
2025-12-05 18:00:28 +0100

    Adjust contents for RML 4.2.0

---
+++ MonkeyLoader.GamePacks.ResoniteModLoader/Settings/ModSettings.cs
@@ -0,0 +1,18 @@
+﻿/*
+Not implemented because it involves creating FrooxEngine components
+and the ML Resonite Integration already has a Settings Category anyways.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResoniteModLoader
+{
+    [AutoRegisterSetting]
+    [SettingCategory("ResoniteModLoader")]
+    public sealed class ModSettings : SettingComponent<ModSettings>
+    {
+        public override bool UserspaceOnly => true;
+    }
+}
+*/
